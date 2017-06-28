c554583598be3c99a19a6e045ef21f9e97c83b66
Johan Kool <johankool@users.noreply.github.com>
2017-06-28 15:04:25 +0200

    Initial Commit

---
+++ EduVPN/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EduVPN
+//
+//  Created by Johan Kool on 28/06/2017.
+//  Copyright © 2017 EduVPN. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
