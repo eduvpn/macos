a04c0838d36c46e1d0cf348a41664ca78eb0e372
Johan Kool <johankool@users.noreply.github.com>
2017-12-16 01:21:54 +0100

    EDUVPNMAC-38: Display messages

--- eduVPN/ConnectionViewController.swift
+++ eduVPN/ConnectionViewController.swift
@@ -20,10 +20,14 @@ class ConnectionViewController: NSViewController {
     @IBOutlet var disconnectButton: NSButton!
     @IBOutlet var connectButton: NSButton!
     @IBOutlet var statisticsBox: NSBox!
+    @IBOutlet var notificationsBox: NSBox!
+    @IBOutlet var notificationsField: NSTextField!
     
     var profile: Profile!
     var authState: OIDAuthState!
     @objc var statistics: Statistics?
+    private var systemMessages: [Message] = []
+    private var userMessages: [Message] = []
     
     @IBOutlet var statisticsController: NSObjectController!
     
@@ -46,26 +50,34 @@ class ConnectionViewController: NSViewController {
         updateForStateChange()
         NotificationCenter.default.addObserver(self, selector: #selector(stateChanged(notification:)), name: ConnectionService.stateChanged, object: ServiceContainer.connectionService)
         
-        // TODO: Display messages
+        // Fetch messages
         ServiceContainer.providerService.fetchMessages(for: profile.info, audience: .system, authState: authState) { (result) in
-            switch result {
-            case .success(let messages):
-                dump(messages)
-            case .failure(let error):
-                dump(error)
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let messages):
+                    self.systemMessages = messages
+                    self.updateMessages()
+                case .failure(let error):
+                    // Ignore
+                    break
+                }
             }
         }
         
         ServiceContainer.providerService.fetchMessages(for: profile.info, audience: .user, authState: authState) { (result) in
-            switch result {
-            case .success(let messages):
-                dump(messages)
-            case .failure(let error):
-                dump(error)
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let messages):
+                    self.userMessages = messages
+                    self.updateMessages()
+                case .failure(let error):
+                    // Ignore
+                    break
+                }
             }
         }
     }
-
+    
     override func viewDidDisappear() {
         super.viewDidDisappear()
         NotificationCenter.default.removeObserver(self, name: ConnectionService.stateChanged, object: ServiceContainer.connectionService)
@@ -156,6 +168,18 @@ class ConnectionViewController: NSViewController {
                     }
                 }
             }
+        }
+    }
+    
+    private func updateMessages() {
+        let messages = userMessages + systemMessages
+        notificationsBox.title = messages.count == 1 ? NSLocalizedString("Notification", comment: "Notification box title (1 message)") : NSLocalizedString("Notifications", comment: "Notifications box title")
+        notificationsBox.isHidden = messages.isEmpty
+        
+        notificationsField.attributedStringValue = messages.reduce(into: NSMutableAttributedString()) { (notifications, message) in
+            let date =  DateFormatter.localizedString(from: message.date, dateStyle: .short, timeStyle: .short)
+            notifications.append(NSAttributedString(string: date + ": ", attributes: [.font: NSFont.boldSystemFont(ofSize: NSFont.systemFontSize(for: .small))]))
+            notifications.append(NSAttributedString(string: message.message + "\n\n", attributes: [.font: NSFont.systemFont(ofSize: NSFont.systemFontSize(for: .small))]))
         }
     }
     
