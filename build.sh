1c53547c3ee4ce24796b6e395b8661bc2ae39b7a
Robin Paul <robin.paul@surfnet.nl>
2018-12-14 16:43:35 +0600

    updated build.sh file
    1) User-friendly
    2) Fixed code signing
    3) Better DMG creation, User can save 24.99$

--- build.sh
+++ build.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 echo "Build Script for eduVPN (and derivatives)"
 
-echo ""
+#Check if HomeBrew Installed
+if ! [ -x "$(command -v brew)" ]; then
+  echo 'Error: Homebrew is not installed. Install Homebrew Manually please ' >&2
+  python -mwebbrowser https://brew.sh
+  exit 1
+
+fi
+
+
+# Check if the Carthage is installed
+if ! [ -x "$(command -v carthage)" ]; then
+  echo 'Carthage is not installed. Installing Carthage' >&2
+  brew install carthage
+fi
+
+
+# Install create dmg
+if ! [ -x "$(command -v create-dmg)" ]; then
+  brew install create-dmg
+fi
+
+
 echo "Which target do you want to build?"
 echo "1. eduVPN"
 echo "2. Let's Connect!"
-read -p "0-9?" choice
+read -p "1-2?" choice
 case "$choice" in
   1 ) TARGET="eduVPN"; PRODUCT="eduVPN.app";;
   2 ) TARGET="LetsConnect"; PRODUCT="Let's Connect!.app";;
@@ -16,12 +37,30 @@ echo ""
 echo "Which signing identity do you want to use?"
 echo "1. SURFnet B.V. (ZYJ4TZX4UU)"
 echo "2. Egeniq (E85CT7ZDJC)"
-echo "3. Other"
-read -p "0-9?" choice
+echo "3. Enter own Team(This isn't just a random name, but it must be exact as in their signing identity): "
+read -p "1-3?" choice
+
+
+# Enter custom Team ID and Team Name.
+if [ "$choice" == 3  ]
+then
+read -p "Enter Team ID: " CUSTOMTEAMID
+read -p "Enter Team Name: " CUSTOMTEAMNAME
+fi
+
+#Simple TeamID Validation. Apple Team ID always consists of 10 Characters
+if  ! [ "${#CUSTOMTEAMID}" == 10  ]
+then
+echo "Error: Team ID is not valid"
+exit 1
+fi
+
+
+
 case "$choice" in
   1 ) TEAMID="ZYJ4TZX4UU"; SIGNINGIDENTITY="Developer ID Application: SURFnet B.V. ($TEAMID)";;
   2 ) TEAMID="E85CT7ZDJC"; SIGNINGIDENTITY="Developer ID Application: Egeniq ($TEAMID)";;
-  3 ) echo "Please adjust the build script to add your signing identity."; exit 0;;
+  3 ) TEAMID="$CUSTOMTEAMID"; SIGNINGIDENTITY="Developer ID Application: $CUSTOMTEAMNAME ($TEAMID)";;
   * ) echo "Invalid response."; exit 0;;
 esac
 
@@ -49,7 +88,7 @@ esac
 FILENAME="$TARGET-$VERSION"
 
 echo ""
-echo "$(tput setaf 2)Bootstrapping dependencies using carthage$(tput sgr 0)"
+echo "Bootstrapping dependencies using carthage"
 # This is a workaround for getting Carthage to work with Xcode 10
 tee ${PWD}/Carthage/64bit.xcconfig <<-'EOF'
 ARCHS = $(ARCHS_STANDARD_64_BIT)
@@ -58,42 +97,44 @@ EOF
 XCODE_XCCONFIG_FILE="${PWD}/Carthage/64bit.xcconfig" carthage bootstrap --cache-builds --platform macOS
 
 echo ""
-echo "$(tput setaf 2)Building and archiving$(tput sgr 0)"
+echo "Building and archiving"
 xcodebuild archive -project eduVPN.xcodeproj -scheme $TARGET -archivePath $FILENAME.xcarchive DEVELOPMENT_TEAM=$TEAMID
 
 echo ""
-echo "$(tput setaf 2)Exporting$(tput sgr 0)"
+echo "Exporting"
 /usr/libexec/PlistBuddy -c "Set :teamID \"$TEAMID\"" ExportOptions.plist
 xcodebuild -exportArchive -archivePath $FILENAME.xcarchive -exportPath $FILENAME -exportOptionsPlist ExportOptions.plist
 
 echo ""
-echo "$(tput setaf 2)Re-signing up and down scripts$(tput sgr 0)"
+echo "Re-signing up and down scripts"
 DOWN=$(find $FILENAME -name "*.down.*.sh" -print)
 codesign -f -s "$SIGNINGIDENTITY" "$DOWN"
 UP=$(find $FILENAME -name "*.up.*.sh" -print)
 codesign -f -s "$SIGNINGIDENTITY" "$UP"
 
 echo ""
-read -p "Create disk image (requires DropDMG license) (y/n)?" choice
-case "$choice" in
-  y|Y ) ;;
-  n|N ) exit 0;;
-  * ) echo "Invalid response."; exit 0;;
-esac
 
-echo ""
-echo "$(tput setaf 2)Creating a disk image$(tput sgr 0)"
-# The configuration eduVPN can be used for all products
-echo "Using: dropdmg --config-name \"eduVPN\" --signing-identity=\"$SIGNINGIDENTITY\" \"$FILENAME/$PRODUCT\""
-dropdmg --config-name "eduVPN" --signing-identity="$SIGNINGIDENTITY" "$FILENAME/$PRODUCT"
+INSTALLERFILENAME="eduVPN-Installer$(date +"%Y-%m-%d-%H:%M:%S.")dmg"
 
-echo ""
-echo "$(tput setaf 2)Creating app cast XML$(tput sgr 0)"
-DISTRIBUTIONPATH="../eduvpn-macos-distrib"
-# Assumptions are being made about the location of this script
-# Also, this often fails due to extended attribute
-echo "Using: $DISTRIBUTIONPATH/generate_appcast $DISTRIBUTIONPATH/dsa_priv.pem $DISTRIBUTIONPATH/updates/"
-$DISTRIBUTIONPATH/generate_appcast $DISTRIBUTIONPATH/dsa_priv.pem $DISTRIBUTIONPATH/updates/
+
+create-dmg \
+--volname "eduVPN" \
+--volicon "icon.icns" \
+--background "background.png" \
+--window-pos 490 350 \
+--window-size 490 350 \
+--icon-size 100 \
+--icon "eduVPN.app" 100 155 \
+--hide-extension "eduVPN.app" \
+--app-drop-link 370 155 \
+ $INSTALLERFILENAME \
+$FILENAME"/eduVPN.app"
+codesign -f -s "$SIGNINGIDENTITY" $INSTALLERFILENAME
 
 echo ""
 echo "Done! You can now upload the files in the updates folders to your file server. Also remember to merge the release branch into master and tag it."
+
+
+
+
+
