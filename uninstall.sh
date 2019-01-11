0708b9b6d7547a54030e5eeb16f966a6a966d1a4
Johan Kool <johankool@users.noreply.github.com>
2019-01-11 16:18:49 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -7,6 +7,9 @@ case "$choice" in
   * ) echo "Invalid response."; exit 0;;
 esac
 
+echo "Removing helper process"
+sudo launchctl remove org.eduvpn.app.openvpnhelper
+
 echo "Deleting /Library/LaunchDaemons/org.eduvpn.app.openvpnhelper.plist"
 sudo rm -f /Library/LaunchDaemons/org.eduvpn.app.openvpnhelper.plist
 
@@ -22,16 +25,4 @@ rm -rf ~/Library/Caches/org.eduvpn.app/
 echo "Deleting ~/Library/Preferences/org.eduvpn.app.plist"
 rm -f ~/Library/Preferences/org.eduvpn.app.plist
 
-echo "Removing helper process, you can now reinstall openvpn helper tool without restarting the MAC"
-sudo launchctl remove org.eduvpn.app.openvpnhelper
-
-echo "Uninstall completed. Reboot your machine for the uninstall to complete."
-echo ""
-echo "Do you want to reboot now?"
-read -p "Reboot (y/n)?" choice
-case "$choice" in 
-  y|Y ) ;;
-  n|N ) echo "Not rebooting. Please reboot later."; exit 0;;
-  * ) echo "Invalid response. Not rebooting. Please reboot later."; exit 0;;
-esac
-sudo reboot
+echo "Uninstall completed."
